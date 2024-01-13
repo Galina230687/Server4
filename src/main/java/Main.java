52449ca5e6e08d192cc56d7aaca4d889e810c62e
Galina <serena_05@mail.ru>
2024-01-13 21:46:52 +0300

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.net.http.HttpClient;
+
 public class Main {
     public static void main(String[] args){
+
+        HttpClient client = (HttpClient) HttpClient.newBuilder();
+
         Server server = new Server(9999);
         server.start();
     }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 public class Server {
 
     private final int port;
